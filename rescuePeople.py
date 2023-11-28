f6c60c6a09640e219f1c44397e0e2dcda1fb3c3a
pabloju23 <132954885+pabloju23@users.noreply.github.com>
2023-11-28 19:20:03 +0100

    Update rescuePeople.py

--- rescuePeople.py
+++ rescuePeople.py
@@ -1,5 +1,8 @@
 from GUI import GUI
 from HAL import HAL
+import cv2
+import math 
+
 
 # Origin: 40º16’48.2” N, 3º49’03.5” W = 430492E, 4459162N
 # Goal: 40º16’47.23” N, 3º49’01.78” W = 430532E, 4459132N
@@ -18,20 +21,43 @@ landing_margin = 0.07
 x_pos = HAL.get_position()[0]
 y_pos = HAL.get_position()[1]
 
+inpos = True
+
+# Parameters for the spiral
+spiral_radius_increment = 0.1  # Increase this value to adjust the spiral spacing
+spiral_angle_increment = 0.1   # Increase this value to adjust the angle between spiral points
+current_spiral_radius = 0.0
+
+
 HAL.takeoff(3)
 
-while not ((victims_x-1 < x_pos) and (x_pos <victims_x+1) and (victims_y-1 < y_pos) and (y_pos < victims_y+1)):
-  	GUI.showImage(HAL.get_frontal_image())
-  	GUI.showLeftImage(HAL.get_ventral_image())
-  	x_pos = HAL.get_position()[0]
-  	y_pos = HAL.get_position()[1]
-  	HAL.set_cmd_pos(victims_x, victims_y, 3, angle)
-  	# time.sleep(0.01)
-  	print('x:', x_pos)
-  	print('y:', y_pos)  
-	
-
+while inpos:
+  GUI.showImage(HAL.get_frontal_image())
+  GUI.showLeftImage(HAL.get_ventral_image())
+  x_pos = HAL.get_position()[0]
+  y_pos = HAL.get_position()[1]
+  HAL.set_cmd_pos(victims_x, victims_y, 3, angle)
+  # time.sleep(0.01)
+  print('x:', x_pos)
+  print('y:', y_pos)  
+  
+  if ((victims_x-0.2 < x_pos) and (x_pos <victims_x+0.2) and (victims_y-0.2 < y_pos) and (y_pos < victims_y+0.2)):
+      inpos = False
+  	
 while True:
-    # Enter iterative code!
+  # Let's go
     GUI.showImage(HAL.get_frontal_image())
     GUI.showLeftImage(HAL.get_ventral_image())
+    
+    # Update the spiral position
+    current_spiral_radius += 0.01
+    spiral_x = victims_x + current_spiral_radius * math.cos(angle)
+    spiral_y = victims_y + current_spiral_radius * math.sin(angle)
+    
+    # Set the drone's position
+    HAL.set_cmd_pos(spiral_x, spiral_y, 3, angle)
+    
+    # Print the current position (for debugging)
+    print('x:', spiral_x)
+    print('y:', spiral_y)
+    print('radius:', current_spiral_radius)
